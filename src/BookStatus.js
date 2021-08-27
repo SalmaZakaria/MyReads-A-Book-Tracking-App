0df981e9c6c38aa0667701fed8fec5c99298e19a
Salma Zakaria <53764024+SalmaZakaria@users.noreply.github.com>
2021-08-27 14:11:38 +0200

    Update BookStatus.js

--- src/BookStatus.js
+++ src/BookStatus.js
@@ -8,51 +8,37 @@ class BookStatus extends Component {
     }
     render() {
         const { Books, ChangingStatus } = this.props;
-        return ( <
-            div >
-            <
-            div className = "bookshelf" >
-            <
-            h2 className = "bookshelf-title" > { this.props.title } < /h2> <
-            div className = "bookshelf-books" >
-            <
-            ol className = "books-grid" > {
+        return ( 
+        <div>
+            <div className = "bookshelf" >
+            <h2 className = "bookshelf-title" > { this.props.title } </h2> 
+            <div className = "bookshelf-books" >
+            <ol className = "books-grid" > {
                 Books.map((book) =>
-                    <
-                    li key = { book.id } >
-                    <
-                    div className = "book" >
-                    <
-                    div className = "book-top" >
-                    <
-                    div className = "book-cover"
-                    style = {
-                        { width: 120, height: 180, backgroundImage: `url(${book.imageLinks.thumbnail})` } } > < /div> <
-                    div className = "book-shelf-changer" >
-                    <
-                    select value = { book.shelf }
-                    onChange = {
-                        (event) => ChangingStatus(book, event.target.value) } >
-                    <
-                    option value = "move"
-                    disabled > Move to... < /option> <
-                    option value = "currentlyReading" > Currently Reading < /option> <
-                    option value = "wantToRead" > Want to Read < /option> <
-                    option value = "read" > Read < /option> <
-                    option value = "none" > None < /option> <
-                    /select> <
-                    /div> <
-                    /div> <
-                    div className = "book-title" > { book.title } < /div> <
-                    div className = "book-authors" > { book.authors } < /div> <
-                    /div> <
-                    /li>                             
+                    <li key = { book.id } >
+                    <div className = "book" >
+                    <div className = "book-top" >
+                    <div className = "book-cover"style = {{ width: 120, height: 180, backgroundImage: `url(${book.imageLinks.thumbnail})` } } > </div> 
+                    <div className = "book-shelf-changer" >
+                    <select value = { book.shelf }onChange = {(event) => ChangingStatus(book, event.target.value) } >
+                    <option value = "move"disabled > Move to... </option> 
+                    <option value = "currentlyReading" > Currently Reading </option>
+                    <option value = "wantToRead" > Want to Read </option>
+                    <option value = "read" > Read </option> 
+                    <option value = "none" > None </option> 
+                    </select> 
+                    </div>
+                    </div>
+                    <div className = "book-title" > { book.title } </div>
+                    <div className = "book-authors" > { book.authors } </div> 
+                    </div> 
+                    </li>                             
                 )
-            } <
-            /ol> <
-            /div> <
-            /div> <
-            /div>
+            }
+            </ol> 
+            </div>
+            </ div>
+            </ div>
         );
     }
 }
